4f86e5ae96d33d6409ba16f0b29e2cca8eedf67f
Abraham Pardo <82788640+pardo024@users.noreply.github.com>
2023-05-20 18:34:15 -0600

    Base de datos

--- conexion.js
+++ conexion.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Conexión a la base de datos MySQL
 async function connectToDatabase() {
   try {
-    const connection = await mysql.createConnection('mysql://18tdpvb7y4p402xxmym1:pscale_pw_VqysoyyTeHydjYTa3mZVZRnq1ZwP3EOCOZ5nWqqcoa8@aws.connect.psdb.cloud/finperbd?ssl={"rejectUnauthorized":true}');
+    const connection = await mysql.createConnection('mysql://om0rcs0m827xivxre8c1:pscale_pw_W7XUYAHcvG1CWmtVMEUv9sSHY4P7VT1Oxt08hcsLtRg@aws.connect.psdb.cloud/finperbd?ssl={"rejectUnauthorized":true}');
 
     return connection;
   } catch (error) {
